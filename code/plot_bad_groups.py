565cb4db0f70c964f7222c5cfe255062b4574de2
Matt George <mattgeorge1@gmail.com>
2012-04-18 19:04:19 +0000

    updated plot_bad_groups.py with Lx limit and aesthetics

--- code/plot_bad_groups.py
+++ code/plot_bad_groups.py
@@ -4,6 +4,7 @@ import fitsio
 import matplotlib
 matplotlib.use('PS')
 import matplotlib.pyplot as plt
+import numpy
 
 file="~/data/cosmos/code/group5_20110914.fits"
 group=fitsio.read(file, ext=1)
@@ -21,23 +22,31 @@ plt.rc('axes',linewidth=1.5)
 
 # start the plot with axes
 plt.figure(1)
-plt.xlim((0,1.03))
+zmax=1.03
+plt.xlim((0,zmax))
 plt.ylim((41.2,44))
 plt.xlabel(r'z',fontsize='medium')
 plt.ylabel(r'log(L$_{\mathrm{X}}$) (erg/s)',fontsize='medium')
-
 
 label1=r'$\mathrm{MMGG_{scale} \ne MMGG_{R200}}$'
 label2=r'$\mathrm{MMGG_{scale} \ne BGG_{scale}}$'
 
 plt.scatter(group['ZPHOT'],group['LX_APP'],c='gray',edgecolors='none',marker='o',s=20)
 plt.scatter(group['ZPHOT'][msmr],group['LX_APP'][msmr],c='r',edgecolors='maroon',facecolors='r',marker='d',s=140,label=label1)
-plt.scatter(group['ZPHOT'][msbs],group['LX_APP'][msbs],c='b',edgecolors='navy',facecolors='b',marker='s',s=70,label=label2)
+plt.scatter(group['ZPHOT'][msbs],group['LX_APP'][msbs],c='b',edgecolors='skyblue',facecolors='b',marker='s',s=70,label=label2)
 
 plt.legend((label1, label2), loc='lower right', scatterpoints=1, markerscale=1, prop={'size':18},handletextpad=0).draw_frame(False)
 ltext=plt.gca().get_legend().get_texts()
 plt.setp(ltext[0],color='red')
 plt.setp(ltext[1],color='blue')
+
+# read in luminosity limits
+groupLimFile="/Users/mgeorge/data/cosmos/auxfiles/lx_z_flm15p0.hst" # shallower limit for 96% of field, inner 52% goes deeper
+zlim,llim=numpy.loadtxt(groupLimFile,unpack=True)
+zlt1=zlim <= zmax
+# 3rd order polynomial fit to measured curve
+poly=numpy.polyfit(zlim,llim,3)
+plt.plot(zlim,poly[0]*zlim**3 + poly[1]*zlim**2 + poly[2]*zlim + poly[3],'k--',linewidth=2)
 
 
 plotDir="/Users/mgeorge/data/cosmos/groups_lensing/plots/"
